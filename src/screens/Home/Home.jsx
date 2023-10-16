b58bf5983b0fc30d5718973a28f9ea348cde2982
Dhaker Achour <achour.dhaker@gmail.com>
2023-10-16 17:12:19 +0100

    Content

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,6 +7,8 @@ import './Home.css';
 import img1 from '../../assets/Home/img1.jpg';
 import img2 from '../../assets/Home/img2.jpg';
 import img3 from '../../assets/Home/img3.jpeg';
+import './Home.css'; // Import your CSS file
+import testimonialImage from '../../assets/Home/testimonial.jpg'
 
 const Home = () => {
     // Sample slider settings
@@ -15,8 +17,8 @@ const Home = () => {
         infinite: true, // Enable infinite loop
         speed: 800, // Transition speed (ms)
         slidesToShow: 1, // Number of slides to show at a time
-        slidesToScroll: 1, 
-        autoplay: true, 
+        slidesToScroll: 1,
+        autoplay: true,
         autoplaySpeed: 3000, // Time between auto-play slides in milliseconds (3 seconds in this case)
     };
 
@@ -55,6 +57,40 @@ const Home = () => {
                         ))}
                     </Slider>
                 </div>
+                <div className="inner_sections">
+                    <div className="services-section">
+                        <h2>Nos Services</h2>
+                        <p>
+                            À notre maison de retraite, nous offrons une gamme de services pour répondre aux besoins
+                            de nos résidents. Ces services comprennent des soins médicaux, des activités de loisirs,
+                            des repas de qualité, et bien plus encore. Nous nous engageons à assurer le bien-être de nos
+                            résidents.
+                        </p>
+                    </div>
+                    <div className="testimonial-section">
+                        <h2>Témoignages de nos Résidents</h2>
+                        <div className="testimonial">
+                            <img src={testimonialImage} alt="Témoignage" />
+                            <p>
+                                "J'ai trouvé une seconde maison ici. Le personnel est incroyablement attentionné, et
+                                je me sens en sécurité et bien soigné."
+                            </p>
+                        </div>
+                        {/* Add more testimonials as needed */}
+                    </div>
+                    <div className="contact-section">
+                        <h2>Contactez-Nous</h2>
+                        <p>
+                            Si vous avez des questions ou si vous souhaitez planifier une visite, n'hésitez pas à nous
+                            contacter. Nous sommes là pour vous aider à prendre la meilleure décision pour vous ou vos
+                            proches.
+                        </p>
+                        <a href="/contact" className="contact-button">
+                            Contactez-nous
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </>
     );
