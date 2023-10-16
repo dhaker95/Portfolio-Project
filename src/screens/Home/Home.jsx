96eddf873e6cecd37f622bc58f251358d8f9cebe
Dhaker Achour <achour.dhaker@gmail.com>
2023-10-16 19:06:25 +0100

    Testimonials and services

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -7,20 +7,23 @@ import './Home.css';
 import img1 from '../../assets/Home/img1.jpg';
 import img2 from '../../assets/Home/img2.jpg';
 import img3 from '../../assets/Home/img3.jpeg';
-import './Home.css'; 
+import './Home.css';
 import testimonialImage from '../../assets/Home/testimonial.jpg'
+import testimonialImage2 from '../../assets/Home/testimonial2.jpg'
+import testimonialImage3 from '../../assets/Home/testimonial3.jpeg'
 import Footer from '../../components/Footer/Footer';
+import servicesImage from '../../assets/Home/services.jpg'
 
 const Home = () => {
     // Sample slider settings
     const sliderSettings = {
-        dots: true, // Show navigation dots
-        infinite: true, // Enable infinite loop
-        speed: 800, // Transition speed (ms)
-        slidesToShow: 1, // Number of slides to show at a time
+        dots: true, 
+        infinite: true, 
+        speed: 800, 
+        slidesToShow: 1,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 3000, // Time between auto-play slides in milliseconds (3 seconds in this case)
+        autoplaySpeed: 3000, 
     };
 
     // Sample slide data
@@ -60,24 +63,46 @@ const Home = () => {
                 </div>
                 <div className="inner_sections">
                     <div className="services-section">
-                        <h2>Nos Services</h2>
-                        <p>
-                            À notre maison de retraite, nous offrons une gamme de services pour répondre aux besoins
-                            de nos résidents. Ces services comprennent des soins médicaux, des activités de loisirs,
-                            des repas de qualité, et bien plus encore. Nous nous engageons à assurer le bien-être de nos
-                            résidents.
-                        </p>
-                    </div>
-                    <div className="testimonial-section">
-                        <h2>Témoignages de nos Résidents</h2>
-                        <div className="testimonial">
-                            <img src={testimonialImage} alt="Témoignage" />
-                            <p>
-                                "J'ai trouvé une seconde maison ici. Le personnel est incroyablement attentionné, et
-                                je me sens en sécurité et bien soigné."
-                            </p>
+                        <div className="services-content">
+                            <div className="services-text">
+                                <h2>Nos Services</h2>
+                                <p>
+                                    À notre maison de retraite, nous offrons une gamme de services pour répondre aux besoins de nos résidents. Ces services comprennent des soins médicaux, des activités de loisirs, des repas de qualité, et bien plus encore. Nous nous engageons à assurer le bien-être de nos résidents.
+                                </p>
+                            </div>
+                            <div className="services-image">
+                                <img src={servicesImage} alt="Services" />
+                            </div>
                         </div>
                     </div>
+
+                    <div className="testimonial-section">
+                        <h2>Témoignages de nos Résidents</h2>
+                        <div className="testimonial-cards">
+                            <div className="testimonial-card">
+                                <img src={testimonialImage} alt="Témoignage" />
+                                <p>
+                                    "J'ai trouvé une seconde maison ici. Le personnel est incroyablement attentionné, et
+                                    je me sens en sécurité et bien soigné."
+                                </p>
+                            </div>
+                            <div className="testimonial-card">
+                                <img src={testimonialImage2} alt="Témoignage" />
+                                <p>
+                                    "Je suis reconnaissante pour l'attention et les soins que j'ai reçus dans cette maison de retraite.
+                                    C'est un endroit spécial."
+                                </p>
+                            </div>
+                            <div className="testimonial-card">
+                                <img src={testimonialImage3} alt="Témoignage" />
+                                <p>
+                                    "La maison de retraite offre un environnement chaleureux et convivial. J'ai fait de belles rencontres ici."
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+
                     <div className="contact-section">
                         <h2>Contactez-Nous</h2>
                         <p>
@@ -92,7 +117,7 @@ const Home = () => {
                 </div>
 
             </div>
-            <Footer/>
+            <Footer />
         </>
     );
 }
