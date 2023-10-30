ce9db631a21490067fadb1a4755130dead6c4b00
Hilya02 <ntindahilya@gmail.com>
2023-10-30 22:05:14 +0200

    Update all new changes

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -1,30 +1,80 @@
-import React from 'react';
-import './Contact.css'
+import React, { useEffect } from 'react';
+import emailjs from 'emailjs-com'; // Import the emailjs library
+import './Contact.css';
 import Header from '../../components/Header/Header';
 import Footer from '../../components/Footer/Footer';
 
 const Contact = () => {
+    // Set up Google Maps when the component mounts
+    useEffect(() => {
+        const googleMap = new window.google.maps.Map(document.getElementById("google-map"), {
+            center: { lat: -34.397, lng: 150.644 }, // Set initial map coordinates
+            zoom: 8, // Set the initial zoom level
+        });
+    }, []);
+
+    // Define your array of sales agents
+    const agents = [
+        {
+            name: "Agent 1",
+            country: "Country 1",
+            phone: "+1234567890",
+            email: "agent1@example.com",
+        },
+        // Add more agents...
+    ];
+
+    // Function to handle sending emails
+    const handleEmail = (agentEmail) => {
+        emailjs.send("your_email_service_id", "your_template_id", {
+            to_email: agentEmail,
+        })
+        .then((response) => {
+            console.log("Email sent!", response);
+        })
+        .catch((error) => {
+            console.error("Email could not be sent:", error);
+        });
+    };
+
     return (
         <>
-        <Header/>
+            <Header />
             <div className="contact-container">
-            <div className="contact-form">
-                <h2>Contact us</h2>
-                <form>
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Enter your name" required />
+                <div className="contact-form">
+                    <h2>Contact us</h2>
+                    <form>
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Enter your name" required />
 
-                    <label htmlFor="email">E-mail</label>
-                    <input type="email" id="email" name="email" placeholder="Enter your e-mail" required />
+                        <label htmlFor="email">E-mail</label>
+                        <input type="email" id="email" name="email" placeholder="Enter your e-mail" required />
 
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" placeholder="Your message..." required></textarea>
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" name="message" placeholder="Your message..." required></textarea>
 
-                    <button type="submit">Send</button>
-                </form>
+                        <button type="submit">Send</button>
+                    </form>
+                </div>
             </div>
-        </div>
-        <Footer/>
+            <div className="map">
+                <div id="google-map" style={{ width: "100%", height: "400px" }}></div>
+            </div>
+            <div className="sales-agents">
+                <h2>Sales Agents</h2>
+                <div className="agent-list">
+                    {agents.map((agent, index) => (
+                        <div key={index} className="agent-card">
+                            <p>Name: {agent.name}</p>
+                            <p>Country: {agent.country}</p>
+                            <p>Phone: {agent.phone}</p>
+                            <p>Email: {agent.email}</p>
+                            <button onClick={() => handleEmail(agent.email)}>Talk to Agent</button>
+                        </div>
+                    ))}
+                </div>
+            </div>
+            <Footer />
         </>
     );
 }
