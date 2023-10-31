04e9bcbfc33844bc9b3c581adf645e529c4fb4b4
Dhaker Achour <achour.dhaker@gmail.com>
2023-10-31 19:07:30 +0100

    About Headers

--- src/screens/Housing/Housing.jsx
+++ src/screens/Housing/Housing.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 const Housing = () => {
     return (
         <>
-            
+            <Header/>
+            <div className="housing">
+
+            </div>
+            <Footer/>
         </>
     );
 }
