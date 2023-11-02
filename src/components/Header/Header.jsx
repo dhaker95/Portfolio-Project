2a306cf7585178bb9f6c0545bfb9dc6bd57d0d5d
Dhaker Achour <achour.dhaker@gmail.com>
2023-11-02 16:30:27 +0100

    Updated Header to link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import logo from '../../assets/Header/logo2.png';
 import './Header.css';
 
@@ -6,14 +7,14 @@ const Header = () => {
     return (
         <div className="header">
             <div className="navbar">
-                <a className="image" href='/'>
+                <Link to="/" className="image"> 
                     <img src={logo} alt="" width="200" height="75" />
-                </a>
+                </Link>
                 <div className="menu">
-                    <a href="/">Home</a>
-                    <a href="about">About us</a>
-                    <a href="contact">Contact</a>
-                    <a href="housing">Housing</a>
+                    <Link to="/">Home</Link> 
+                    <Link to="/about">About us</Link>
+                    <Link to="/contact">Contact</Link>
+                    <Link to="/housing">Housing</Link>
                 </div>
                 <button className="explore-button">Explore</button>
             </div>
